282246ad5f77af96697a2583c6f79c9a9dc684da
Anderson Silva <anderson.oliveirasilva@gmail.com>
2021-04-27 08:30:40 -0300

    Criando o repositório do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,57 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoriasComponent } from './categorias/categorias.component';
+import { CategoriaDetalheComponent } from './categoria-detalhe/categoria-detalhe.component';
+import { CategoriaNovaComponent } from './categoria-nova/categoria-nova.component';
+import { CategoriaEditarComponent } from './categoria-editar/categoria-editar.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuComponent } from './menu/menu.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+
+import {  
+  MatCardModule,
+  MatInputModule,
+  MatProgressSpinnerModule,
+  MatSelectModule,
+  MatTableModule} from '@angular/material';
+
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    CategoriasComponent,
+    CategoriaDetalheComponent,
+    CategoriaNovaComponent,
+    CategoriaEditarComponent,
+    LoginComponent,
+    LogoutComponent,
+    MenuComponent
+  ],  
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,  
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    MatIconModule,
+    MatListModule,
+    MatProgressSpinnerModule, 
+    MatSelectModule,
+    MatSidenavModule,  
+    MatTableModule,
+    MatToolbarModule,
+    LayoutModule
+  ],  
   providers: [],
   bootstrap: [AppComponent]
 })
